d31ac81ed8ad2e599ef735d2bb1eaf584c0e88ec
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-06-04 22:12:26 -0700

    created select query with id parameter

--- database/index.js
+++ database/index.js
@@ -1,4 +1,18 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
-
 const connection = mysql.createConnection(mysqlConfig);
+
+const getHousePics = (param, callback) => {
+  const q = 'SELECT * FROM photo WHERE house_id = ' + param;
+  connection.query(q, (error, results, fields) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, results);
+    }
+  });
+}
+
+module.exports = {
+  getHousePics
+};
