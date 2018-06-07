b4d9673f2347d9568d9a7e4f666e4895364bb670
Cameron Streeter <cameron@Camerons-MacBook-Pro.local>
2018-06-06 21:14:27 -0700

    Changed for Airbnb style guide

--- database/index.js
+++ database/index.js
@@ -1,18 +1,19 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
+
 const connection = mysql.createConnection(mysqlConfig);
 
 const getHousePics = (param, callback) => {
-  const q = 'SELECT * FROM photo WHERE house_id = ' + param;
-  connection.query(q, (error, results, fields) => {
+  const q = `SELECT * FROM photo WHERE house_id = ${param}`;
+  connection.query(q, (error, results) => {
     if (error) {
       callback(error, null);
     } else {
       callback(null, results);
     }
   });
-}
+};
 
 module.exports = {
-  getHousePics
+  getHousePics,
 };
